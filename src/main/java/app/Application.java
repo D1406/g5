9101bcfc34b15cd9c1aa083ae5797b3e76880c59
Дмитрий Янковский <d8388388@yandex.ru>
2022-12-11 13:28:48 +0300

    window works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,12 +1,25 @@
 package app;
 
-import io.github.humbleui.jwm.Event;
-
+import io.github.humbleui.jwm.*;
 import java.util.function.Consumer;
 
 public class Application implements Consumer<Event> {
+    private final Window window;
+    public Application() {
+        // создаём окно
+        window = App.makeWindow();
+        // задаём обработчиком событий текущий объект
+        window.setEventListener(this);
+        // делаем окно видимым
+        window.setVisible(true);
+    }
     @Override
-    public void accept(Event event) {
-
+    public void accept(Event e) {
+        if (e instanceof EventWindowClose) {
+            // завершаем работу приложения
+            App.terminate();
+        }else if (e instanceof EventWindowCloseRequest) {
+            window.close();
+        }
     }
 }
