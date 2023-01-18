8d1efc3b341c31cee000db924c905247b99f917e
Дмитрий Янковский <d8388388@yandex.ru>
2023-01-18 19:37:07 +0300

    input select works

--- src/main/java/controls/Input.java
+++ src/main/java/controls/Input.java
@@ -31,7 +31,12 @@ public class Input extends GridPanel {
      * Цвет текста
      */
     private final int textColor;
-
+    /**
+     * флаг, помещён ли сейчас фокус на это поле ввода
+     * (модификатор доступа по умолчанию, чтобы был доступен
+     * фабрике InputFactory внутри пакета)
+     */
+    boolean focused = false;
 
     /**
      * Панель на сетке
@@ -94,7 +99,7 @@ public class Input extends GridPanel {
                 // рисуем линию текста
                 canvas.drawTextLine(line, 0, 0, paint);
                 // если время рисовать курсор
-                if (InputFactory.cursorDraw()) {
+                if (focused && InputFactory.cursorDraw()) {
                     // смещаем область рисования
                     canvas.translate(line.getWidth(), 0);
                     // рисуем его
@@ -226,5 +231,22 @@ public class Input extends GridPanel {
      */
     public String getText() {
         return text;
+    }
+    /**
+     * Установить фокус на это поле ввода
+     */
+    public void setFocus() {
+        // снимаем фокус со всех полей ввода
+        InputFactory.defocusAll();
+        // выделяем текущее поле ввода
+        this.focused = true;
+    }
+    /**
+     * Возвращает флаг, установлен ли фокус на это поле ввода
+     *
+     * @return флаг
+     */
+    public boolean isFocused() {
+        return focused;
     }
 }
